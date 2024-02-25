8bf5ebc5b202432ddc92650b09abac74b31f49db
K-Kuznetsov <kimmok1990@gmail.com>
2024-02-25 03:30:24 +0200

    Changed data model

--- Sqlite_Create.ts
+++ Sqlite_Create.ts
@@ -1,32 +1,32 @@
 import sqlite3 from 'sqlite3';
 const db = new sqlite3.Database('./Kinnisvara.db');
 
-async function SqliteCreate(Table: string): Promise<void> {
+async function SqliteCreate(Table: string): Promise<any> {
     db.run(`DROP TABLE IF EXISTS ${Table}`);
     db.run(`CREATE TABLE IF NOT EXISTS ${Table} (
         ID INTEGER PRIMARY KEY AUTOINCREMENT, 
         Area text, 
         Address text, 
+        EHRCode string,
+        Year integer,
+        EnergyClass text, 
         Rooms integer, 
+        Floor integer, 
+        Floors integer, 
         Size integer, 
         Price integer,
         Latitude real, 
         Longitude real, 
-        FromWork real, 
+        FromWork real,
+        Purpose string,
         Website text, 
-        BuildingType text,
-        Year integer, 
-        Condition text, 
-        EnergyClass text, 
-        Floors integer, 
-        Floor integer, 
+        BuildingType text,         
+        Condition text,         
         HVAC text, 
         Kitchen text, 
         Bathroom text,         
         Technical text,
-        Other text,
-        EHRCode string,
-        Purpose string
+        Other text        
         )`
     );
 };
